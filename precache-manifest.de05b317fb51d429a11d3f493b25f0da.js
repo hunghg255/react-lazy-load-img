8eb0b19b87d9b518f53bec78792432b85ce804c9
giahungbk <giahung197bg@gmail.com>
2020-04-16 16:42:44 +0700

    Updates

---
+++ precache-manifest.de05b317fb51d429a11d3f493b25f0da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1542cb376f37422f6c6b3937e976d89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4e6430d1e702f8fe964",
+    "url": "/static/css/main.dc4f7d1a.chunk.css"
+  },
+  {
+    "revision": "36d48264ef590e112029",
+    "url": "/static/js/2.73dd4241.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.73dd4241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4e6430d1e702f8fe964",
+    "url": "/static/js/main.1d1415eb.chunk.js"
+  },
+  {
+    "revision": "05e7374c0f1306d3f169",
+    "url": "/static/js/runtime-main.26633284.js"
+  }
+]);
