ffb24bb41ed4b171be5fddeef1a96b4ea1a72d73
giahungbk <giahung197bg@gmail.com>
2020-04-16 16:37:47 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+import LazyLoadImage from './components/LazyLoadImage'
+
+
 function App() {
+  const [listImage, setListImage] = useState([]);
+  useEffect(() => {
+    const fetchImg = async () => {
+      let listImg = await fetch('https://picsum.photos/v2/list').then(res => res.json());
+      setListImage(listImg);
+    }
+
+    fetchImg();
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div className="scroll">
+            <h1>Lazyload image</h1>
+          </div>
+          <div className="img">
+            {listImage.map((img, index) => (
+              <div key={index} className="box">
+                <LazyLoadImage
+                  src={img.download_url}
+                  alt={img.author}
+                />
+              </div>
+              ))}
+          </div>
+      </div>
   );
 }
 
