9ef801d8596835ca60b72c65f783f7c82741a9cd
giahungbk <giahung197bg@gmail.com>
2020-04-16 18:14:40 +0700

    Updates

---
+++ precache-manifest.ec0c5095adf36bf4b49b199ac08f1f91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55e03331a3509676c51d34902d71ed3",
+    "url": "/lazy-load-img/index.html"
+  },
+  {
+    "revision": "f4e6430d1e702f8fe964",
+    "url": "/lazy-load-img/static/css/main.dc4f7d1a.chunk.css"
+  },
+  {
+    "revision": "36d48264ef590e112029",
+    "url": "/lazy-load-img/static/js/2.73dd4241.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lazy-load-img/static/js/2.73dd4241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4e6430d1e702f8fe964",
+    "url": "/lazy-load-img/static/js/main.1d1415eb.chunk.js"
+  },
+  {
+    "revision": "f0483ca39000153a6396",
+    "url": "/lazy-load-img/static/js/runtime-main.ef0c9b93.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de05b317fb51d429a11d3f493b25f0da.js"
+  "/lazy-load-img/precache-manifest.ec0c5095adf36bf4b49b199ac08f1f91.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lazy-load-img/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
